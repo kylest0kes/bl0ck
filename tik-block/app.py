b805510a2f8e993a4612c3e879c707a994a0d3d9
kylest0kes <kylestokes1551@gmail.com>
2024-05-11 16:35:50 -0400

    start of project, init of various files

---
+++ tik-block/app.py
@@ -0,0 +1,16 @@
+from selenium import webdriver 
+import sys
+# for cross compatibility with python 2/3
+if sys.version[0] == '3': raw_input=input 
+
+class Blocker():
+    def __init__(self, email, password, wait):
+        self.Driver = webdriver.Firefox()
+        self.email = email
+        self.password = password
+        self.profile_name = None 
+        self.wait = wait
+    
+if __name__ == '__main__':
+    email = raw_input("Please enter your TikTok email: ")
+    print(f"User email: {email}")
