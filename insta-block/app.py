135f8301fd07a670485b0cb761b0f97e870b341a
kylest0kes <kylestokes1551@gmail.com>
2024-05-11 20:46:47 -0400

    set up a block_list file for Instagram with a starter list object. Imported that object into the app file and can read data from it

--- insta-block/app.py
+++ insta-block/app.py
@@ -1,3 +1,4 @@
+from block_list import to_be_blocked
 from selenium import webdriver 
 from selenium.webdriver.common.by import By
 from bs4 import BeautifulSoup
@@ -9,7 +10,7 @@ import sys
 if sys.version[0] == '3': raw_input=input 
 
 class Blocker():
-    def __init__(self, username, password, wait=2):
+    def __init__(self, username, password, wait=6):
         self.driver = webdriver.Firefox()
         self.username = username
         self.password = password
@@ -19,24 +20,23 @@ class Blocker():
         self.driver.quit()
         
     def login(self):
-        self.driver.get('https://www.instagram.com')
-        time.sleep(self.wait)
+        self.driver.get('https://www.instagram.com/')
+        sleep(self.wait)
         username_el = self.driver.find_element("name", "username")
         password_el = self.driver.find_element("name", "password")
         username_el.send_keys(self.username)
         password_el.send_keys(self.password)
         password_el.submit()
         
-        time.sleep(self.wait)
+        sleep(self.wait)
 
         nn_btn = self.driver.find_element(By.CLASS_NAME, "x1yc6y37")
         nn_text_val = nn_btn.text
     
         if nn_text_val == 'Not now':
-            print('nn btn present')
             nn_btn.click()
             
-        time.sleep(self.wait)
+        sleep(self.wait)
             
         notificaitons_nn_btn = self.driver.find_element(By.CLASS_NAME, "_a9_1")
         notificaitons_nn_value = notificaitons_nn_btn.text
@@ -44,20 +44,20 @@ class Blocker():
         if notificaitons_nn_value == 'Not Now':
             notificaitons_nn_btn.click()
 
-        time.sleep(self.wait)
-        
+        sleep(self.wait)
         print('should be logged in')
+        
+    def cycle_block_list(self):
+        for i in to_be_blocked:
+            print(i['url'])
 
 if __name__ == '__main__':
-    from argparse import ArgumentParser
-    import getpass
-    import time
-    
-    parser = ArgumentParser(description='Block everyone on the blocklist on your Instagram. Requires Firefox')
-    parser.add_argument('--wait', type=float, default=1, help="Explicit wait time between page loads (default 2 seconds)")
+    parser = ArgumentParser(description='Block everyone on the block_list on your Instagram. Requires Firefox')
+    parser.add_argument('--wait', type=float, default=3, help="Explicit wait time between page loads (default 6 seconds to be safe)")
     args = parser.parse_args()
     
     username = input("Please enter your Instagram username: ")
     password = getpass.getpass('Please enter your Instagram password: ')
     blocker = Blocker(username=username, password=password, wait=args.wait)
     blocker.login()
+    blocker.cycle_block_list()
---
+++ insta-block/block_list.py
@@ -0,0 +1,22 @@
+to_be_blocked = [
+    {
+        'name': 'Tom Brady',
+        'url': 'https://www.instagram.com/tombrady/'
+    },
+    {
+        'name': 'Kim Kardashian',
+        'url': 'https://www.instagram.com/kimkardashian/'
+    },
+    {
+        'name': 'Ellen DeGeneres',
+        'url': 'https://www.instagram.com/ellendegeneres/'
+    },
+    {
+        'name': 'Marilyn Manson',
+        'url': 'https://www.instagram.com/marilynmanson/'
+    },
+    {
+        'name': 'Kylie Jenner',
+        'url': 'https://www.instagram.com/kyliejenner/'
+    }
+]
