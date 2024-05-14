e341d67e1c09d75c871d44a7d6f09ffbe1b4026e
kylest0kes <kylestokes1551@gmail.com>
2024-05-14 18:13:06 -0400

    removed accidentally committed file

--- quick.py
+++
@@ -1,5 +0,0 @@
-arr = "x1i10hfl xjbqb8w x1ejq31n xd10rxx x1sy0etr x17r0tee x972fbf xcfux6l x1qhh985 xm0m39n x9f619 x1ypdohk xe8uvvx xdj266r xat24cr x1mh8g0r xexx8yu x4uap5 x18d9i69 xkhd6sd x16tdsg8 x1hl2dhg xggy1nq x1o1ewxj x3x9cwd x1e5q0jg x13rtm0m x1n2onr6 x87ps6o x1lku1pv x1a2a7pz x78zum5 x1emribx"
-
-new = arr.replace(" ", ".")
-
-print(new)
