d68e2aa6d3641d84936ea87b5e0cf1c96e97c93c
kylest0kes <kylestokes1551@gmail.com>
2024-05-14 18:19:53 -0400

    set the 'profile_name' value on the Blocker class to grab the users name from the timeline element

--- faceblock/app.py
+++ faceblock/app.py
@@ -32,12 +32,11 @@ class Blocker():
         password_element = self.driver.find_element(By.ID, 'pass')
         password_element.send_keys(self.password)
         password_element.submit()
+        print("Logging in...")
         
         sleep(self.wait)
-        username = self.driver.find_element(By.CSS_SELECTOR, 'x1i10hfl.xjbqb8w.x1ejq31n.xd10rxx.x1sy0etr.x17r0tee.x972fbf.xcfux6l.x1qhh985.xm0m39n.x9f619.x1ypdohk.xe8uvvx.xdj266r.xat24cr.x1mh8g0r.xexx8yu.x4uap5.x18d9i69.xkhd6sd.x16tdsg8.x1hl2dhg.xggy1nq.x1o1ewxj.x3x9cwd.x1e5q0jg.x13rtm0m.x1n2onr6.x87ps6o.x1lku1pv.x1a2a7pz.x78zum5.x1emribx').get_attribute('aria-label')
-        # username_val = username
-        
-        print(username)
+        self.profile_name = self.driver.find_element(By.CSS_SELECTOR, '.x1i10hfl.xjbqb8w.x1ejq31n.xd10rxx.x1sy0etr.x17r0tee.x972fbf.xcfux6l.x1qhh985.xm0m39n.x9f619.x1ypdohk.xe8uvvx.xdj266r.xat24cr.x1mh8g0r.xexx8yu.x4uap5.x18d9i69.xkhd6sd.x16tdsg8.x1hl2dhg.xggy1nq.x1o1ewxj.x3x9cwd.x1e5q0jg.x13rtm0m.x1n2onr6.x87ps6o.x1lku1pv.x1a2a7pz.x78zum5.x1emribx').get_attribute('aria-label').split("'")[0]
+
         
     def cycle_block_list(self):
         pass
