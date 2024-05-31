85b6c3dd76d358b55f7af8dd798181796b95aa40
kylest0kes <kylestokes1551@gmail.com>
2024-05-31 18:23:58 -0400

    completed error handling, refactored some of the Instagram script, tested and confirmed its working

--- instablock/instablock.py
+++ instablock/instablock.py
@@ -20,13 +20,11 @@ class Blocker():
         self.username = username
         self.password = password
         self.wait = wait
-        self.loggedin = None
     
-    def quit(self):
+    def quit_driver(self):
         self.driver.quit()
         
     def login(self):
-        self.loggedin = False
         self.driver.get('https://www.instagram.com/')
         print("Attempting to log in...")
         sleep(4)
@@ -35,39 +33,32 @@ class Blocker():
         username_el.send_keys(self.username)
         password_el.send_keys(self.password)
         password_el.submit()
-        sleep(12)
-        self.driver.get('https://www.instagram.com')
-        sleep(2)
-        account = self.driver.find_element(By.CSS_SELECTOR, ".x1i10hfl.xjbqb8w.x1ejq31n.xd10rxx.x1sy0etr.x17r0tee.x972fbf.xcfux6l.x1qhh985.xm0m39n.x9f619.x1ypdohk.xt0psk2.xe8uvvx.xdj266r.x11i5rnm.xat24cr.x1mh8g0r.xexx8yu.x4uap5.x18d9i69.xkhd6sd.x16tdsg8.x1hl2dhg.xggy1nq.x1a2a7pz.x5n08af.xwhw2v2.x6ikm8r.x10wlt62.xlyipyv.x9n4tj2._a6hd")
+        sleep(self.wait)
+        account = self.driver.find_element(By.CSS_SELECTOR, ".xpdipgo.x972fbf.xcfux6l.x1qhh985.xm0m39n.xk390pu.x5yr21d.xdj266r.x11i5rnm.xat24cr.x1mh8g0r.xl1xv1r.xexx8yu.x4uap5.x18d9i69.xkhd6sd.x11njtxf.xh8yej3").get_attribute('alt')
         
         if account:
-            self.loggedin = True
             print('Logged in :)')
-        else:
-            print("Error logging in. Please try again :)")
         
     def cycle_block_list(self):
         print('Beginning block cycle')
         for i in to_be_blocked:
             self.driver.get(i['url'])
             sleep(self.wait)
-            elipse = self.driver.find_element(By.CLASS_NAME, "xurb0ha")
+            elipse = self.driver.find_element(By.CSS_SELECTOR, ".x1i10hfl.x972fbf.xcfux6l.x1qhh985.xm0m39n.x9f619.xe8uvvx.xdj266r.x11i5rnm.xat24cr.x1mh8g0r.x16tdsg8.x1hl2dhg.xggy1nq.x1a2a7pz.x6s0dn4.xjbqb8w.x1ejq31n.xd10rxx.x1sy0etr.x17r0tee.x1ypdohk.x78zum5.xl56j7k.x1y1aw1k.x1sxyh0.xwib8y2.xurb0ha.xcdnw81")
             elipse.click()
-            sleep(2)
-            block_btns = self.driver.find_elements(By.CLASS_NAME, "_a9--")
+            sleep(self.wait)
+            block_btns = self.driver.find_elements(By.CSS_SELECTOR, ".xjbqb8w.x1qhh985.xcfux6l.xm0m39n.x1yvgwvq.x13fuv20.x178xt8z.x1ypdohk.xvs91rp.x1evy7pa.xdj266r.x11i5rnm.xat24cr.x1mh8g0r.x1wxaq2x.x1iorvi4.x1sxyh0.xjkvuk6.xurb0ha.x2b8uid.x87ps6o.xxymvpz.xh8yej3.x52vrxo.x4gyw5p.xkmlbd1.x1xlr1w8")
             block_btn = None
             for b in block_btns:
                 if b.text == 'Block':
                     block_btn = b
             block_btn.click()
-            sleep(2)
-            block_confirm = self.driver.find_element(By.CLASS_NAME, "x1xlr1w8")
+            sleep(self.wait)
+            block_confirm = self.driver.find_elements(By.CSS_SELECTOR, ".xjbqb8w.x1qhh985.xcfux6l.xm0m39n.x1yvgwvq.x13fuv20.x178xt8z.x1ypdohk.xvs91rp.x1evy7pa.xdj266r.x11i5rnm.xat24cr.x1mh8g0r.x1wxaq2x.x1iorvi4.x1sxyh0.xjkvuk6.xurb0ha.x2b8uid.x87ps6o.xxymvpz.xh8yej3.x52vrxo.x4gyw5p.xkmlbd1.x1xlr1w8")[3]
             block_confirm.click()
-            sleep(2)
-            dismiss_btn = self.driver.find_element(By.CLASS_NAME, "x52vrxo")
-            dismiss_btn.click()
+            sleep(self.wait)
             print(f"{i['name']} is now blocked :)")
-            sleep(2)
+            sleep(self.wait)
         print('Block cycle complete :)')
             
 
@@ -85,8 +76,10 @@ def igb():
 
             blocker.login()
             blocker.cycle_block_list()
-            blocker.quit()
+            blocker.quit_driver()
             break
         except Exception as e:
             print(f"An error occurred. Waiting for 4 seconds and trying again.")
+            print(f"Error: {e}")
+            blocker.quit_driver()
             sleep(4)
